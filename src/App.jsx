69885e52c7818f3d1a6065c9c588c7a483558919
Michael Murray <mcmurray0125@gmail.com>
2023-06-20 20:00:33 -0500

    switch from cra to vite

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Content from "./components/Content"
+import Footer from "./components/Footer"
+import Header from "./components/Header"
+import Navigation from "./components/Navigation"
+
+function App() {
+
+  return (
+    <>
+      <Navigation/>
+      <Header/>
+      <Content/>
+      <Footer/>
+    </>
+  )
+}
+
+export default App
