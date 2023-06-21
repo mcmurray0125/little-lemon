b510e5d1fad53ba839f6fbb6506c04124b2da3a2
Michael Murray <mcmurray0125@gmail.com>
2023-06-20 20:46:15 -0500

    created navbar and footer, edited html meta tags.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,69 @@
 import React from 'react'
+import Container from 'react-bootstrap/Container'
+import Col from 'react-bootstrap/Col'
+import Row from 'react-bootstrap/Row'
 
 export default function Footer() {
   return (
-    <div>Footer</div>
+    <footer className="bg-light text-center">
+      <Container className='py-4'>
+        <Row>
+          <Col sm={3}>
+            <div>
+              <img
+                src="path_to_your_image.png"
+                alt="Vertical Image"
+                style={{ height: '150px' }}
+              />
+            </div>
+          </Col>
+          <Col sm={3}>
+            <div className='text-start'>
+              <h5>Doormat Navigation</h5>
+              <ul className="list-unstyled">
+                <li>
+                  <a href="/">Home</a>
+                </li>
+                <li>
+                  <a href="/">About</a>
+                </li>
+                <li>
+                  <a href="/">Menu</a>
+                </li>
+                <li>
+                  <a href="/">Reservations</a>
+                </li>
+                <li>
+                  <a href="/r">Online Order</a>
+                </li>
+                <li>
+                  <a href="/">Login</a>
+                </li>
+              </ul>
+            </div>
+          </Col>
+          <Col sm={3}>
+            <div className='text-start'>
+              <h5>Contact</h5>
+              <ul className="list-unstyled">
+                <li><a href='/'>Address</a></li>
+                <li><a href='/'>Phone Number</a></li>
+                <li><a href='/'>Email</a></li>
+              </ul>
+            </div>
+          </Col>
+          <Col sm={3}>
+            <div className='text-start'>
+              <h5>Social Media Links</h5>
+              <ul className="list-unstyled">
+                <li><a href='/'>Address</a></li>
+                <li><a href='/'>Phone Number</a></li>
+                <li><a href='/'>Email</a></li>
+              </ul>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
   )
 }
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,32 +1,31 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import logo from "../assets/Logo.png"
 
 export default function Navigation() {
   return (
-    <Navbar expand="lg" className="bg-body-tertiary">
-        <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+    <Navbar expand="lg">
+      <Container >
+        <Navbar.Brand href="#">
+          <img
+            src={logo}
+            height="45"
+            alt="Little Lemon Logo"
+          />
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
+          <Nav className="ms-auto">
             <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">
-                Another action
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">
-                Separated link
-                </NavDropdown.Item>
-            </NavDropdown>
-            </Nav>
+            <Nav.Link href="#about">About</Nav.Link>
+            <Nav.Link href="#menu">Menu</Nav.Link>
+            <Nav.Link href="#reservations">Reservations</Nav.Link>
+            <Nav.Link href="#order-online">Order Online</Nav.Link>
+            <Nav.Link href="#login">Login</Nav.Link>
+          </Nav>
         </Navbar.Collapse>
-        </Container>
+      </Container>
     </Navbar>
   )
 }
